bda053ad84f96e106865acfd461c89a73d9889dd
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-05 21:14:44 +0530

    Q4

--- Abstraction/PracticeQuestions/Q4.java
+++ Abstraction/PracticeQuestions/Q4.java
@@ -7,6 +7,21 @@ abstract class Telephone{
   abstract void liftin();
 
 }
+class SmartTelephone extends Telephone{
+    @Override
+    void ring(){
+        System.out.println("Ringing");
+    }
+    @Override
+    void disconnect(){
+        System.out.println("Call disconnected");
+    }
+    @Override
+    void liftin(){
+        System.out.println("Call picked!");
+    }
+
+}
 
 public class Q4 {
     static void main(String[] args) {
