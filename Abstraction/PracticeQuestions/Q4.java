530ed4d4c3ff8ceaadbab70f6d5725b0ba0a262f
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-05 21:11:30 +0530

    Q4

---
+++ Abstraction/PracticeQuestions/Q4.java
@@ -0,0 +1,15 @@
+package Abstraction.PracticeQuestions;
+
+abstract class Telephone{
+
+   abstract void ring();
+    abstract void disconnect();
+  abstract void liftin();
+
+}
+
+public class Q4 {
+    static void main(String[] args) {
+
+    }
+}
