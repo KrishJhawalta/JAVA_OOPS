12bd9d02f6aed33fa0c9961b15731cee5c2e1cee
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-06 22:26:03 +0530

    Q5 ( Super keyword ),

--- Inheritance/PracticeQuestions/Question5.java
+++ Inheritance/PracticeQuestions/Question5.java
@@ -4,12 +4,19 @@ class krish{
         System.out.println("i am krish");
     }
     krish(int a){
-        System.out.println("i am in overloaded constructor");
+        System.out.println("i am in overloaded constructor" + a);
     }
 }
 
 
-
+class child extends krish{
+    child(){
+        System.out.println("i am in child");
+    }
+    child(int a,int b){
+        System.out.println("i am in overloaded constr");
+    }
+}
 
 public class Question5 {
     public static void main(String[] args) {
