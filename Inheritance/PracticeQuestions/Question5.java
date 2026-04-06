50ace8f422438a49b2b17bd76dd788a62f833ce1
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-06 22:22:48 +0530

    Q5 ( Super keyword )

---
+++ Inheritance/PracticeQuestions/Question5.java
@@ -0,0 +1,18 @@
+package Inheritance.PracticeQuestions;
+class krish{
+    krish(){
+        System.out.println("i am krish");
+    }
+    krish(int a){
+        System.out.println("i am in overloaded constructor");
+    }
+}
+
+
+
+
+public class Question5 {
+    public static void main(String[] args) {
+
+    }
+}
