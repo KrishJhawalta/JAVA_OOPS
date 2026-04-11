c72f71f8132e874e8038103850028f7a3812f2f9
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-11 22:21:51 +0530

    Interfaces

--- Abstraction/Interfaces/How_To_Interface.java
+++ Abstraction/Interfaces/How_To_Interface.java
@@ -1,12 +1,20 @@
 package Abstraction.Interfaces;
 
+interface Animal{
+    void Sound();
+}
+class Dog implements Animal{
+   public void Sound(){
+        System.out.println("Barkk");
+    }
 
-
+}
 public class How_To_Interface {
     public static void main(String[]args) {
-
         // What is an interface?
         //:It is a blueprint that contains the name of methods having (empty body).
+        Dog d1 = new Dog();
+        d1.Sound();
 
 
 
