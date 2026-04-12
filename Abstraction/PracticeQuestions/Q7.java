996f43f56cda71e8f3524893f52f9dd85aba8ada
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-12 21:30:35 +0530

    Questions updated newly

--- Abstraction/PracticeQuestions/Q7.java
+++ Abstraction/PracticeQuestions/Q7.java
@@ -1,21 +1,28 @@
 package Abstraction.PracticeQuestions;
 
-interface Tv1Remote{
-    void Remote();
-}
+ // interfaces 1
+interface SmartR{
+    void Remote1();}
 
+// interfaces 2
+interface Tv1Remote extends SmartR{
+    void Remote();}
+
+// TV class
 class TV implements Tv1Remote{
     public void Remote(){
-        System.out.println("TV Remote working inside TV class");
-    }
-}
+        System.out.println("TV Remote working inside TV class"); }
+
+    public void Remote1(){
+        System.out.println("Working Smartly");
+    } }
 
 public class Q7 {
     public static void main(String[] args) {
 
-        // Objects
+        // Objects of interfaces and TV clas
         TV t = new TV();
         t.Remote();
+        t.Remote1();
 
-    }
-}
+    } }
