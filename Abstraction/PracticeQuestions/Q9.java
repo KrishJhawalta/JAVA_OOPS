413c555d4d991c1b0fc13c34b9b9880a2ee01b59
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-14 21:35:32 +0530

    Interface Question Q9

---
+++ Abstraction/PracticeQuestions/Q9.java
@@ -0,0 +1,25 @@
+package Abstraction.PracticeQuestions;
+
+interface Bicycle{
+    void ApplyBrakes();
+    void ApplyAccelerator();
+}
+
+class KJCycles implements Bicycle{
+    public void ApplyBrakes(){
+        System.out.println("Bicycles ");
+    }
+
+    public void ApplyAccelerator(){
+        System.out.println("Speeping up");
+
+    }
+}
+
+
+public class Q9 {
+   public static void main(String[] args) {
+
+
+    }
+}
