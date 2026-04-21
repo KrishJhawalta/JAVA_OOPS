40f2c2ade3f922d4c43030864089f1e6557d4509
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-21 13:57:01 +0530

    Pattern

--- Abstraction/PracticeQuestions/reverseP.java
+++ Abstraction/PracticeQuestions/reverseP.java
@@ -3,6 +3,8 @@ package Abstraction.PracticeQuestions;
 public class reverseP {
    public static void main(String[] args) {
 
+
+       // loop for pattern printing star
        for(int i=1; i<=5; i++){
            for(int j=5; j>=i; j--){
                System.out.print(" * ");
