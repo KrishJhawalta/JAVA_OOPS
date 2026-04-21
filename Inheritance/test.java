48ae4f2cc4e13bc00c567ccc0a89d48bbed9392f
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-21 13:58:53 +0530

    Calculator

--- Inheritance/test.java
+++ Inheritance/test.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class test {
     static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // scanner class
         int a =  sc.nextInt();
         int b = sc.nextInt();
         int c = sc.next().charAt(0);
 
-        switch(c){
+        switch(c){    // switch statement for operators
             case '+':
                 System.out.println(a+b);
                 break;
