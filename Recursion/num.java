53fda6f9e476578451552a1a9fcf54d46c48fbf2
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-22 21:56:15 +0530

    Recursion.

--- Recursion/num.java
+++ Recursion/num.java
@@ -1,11 +1,12 @@
 package Recursion;
 
+
 public class num {
     void fun(int n){
-        if (n>0){
-            System.out.println(n);
-fun(n-1);
-            System.out.println(n);
+        if (n>0){           // condition 1
+            System.out.println(n);     // 2
+fun(n-1);                         // 3
+            System.out.println(n);     // 4
         }
     }
     public static void main(String[] args) {
