b43863c1f035cfcc6d5aae190f4de3c1d13de9a4
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-23 22:07:36 +0530

    Recursion test

---
+++ Recursion/test.java
@@ -0,0 +1,15 @@
+public class test{
+   public static void main(String[] args) {
+
+     System.out.println("Hello!");
+     void print(int n){
+       if(n>5) return;
+       System.out.println(n);
+       print(n+1);
+     }
+
+    
+  
+    
+  }
+}
