720862a0a6f93bd9cea1e5897906fae9b2cb7e33
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-24 21:51:38 +0530

    File add

--- Recursion/test.java
+++ Recursion/test.java
@@ -2,8 +2,10 @@ public class test{
    public static void main(String[] args) {
 
      System.out.println("Hello!");
+
+      //function
      void print(int n){
-       if(n>5) return;
+       if(n>5) return; //condition for 5
        System.out.println(n);
        print(n+1);
      }
