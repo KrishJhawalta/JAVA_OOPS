754f6c671f75e0d0a76a198f3d3cad2249b91f73
Krish Jhawalta <krishjhawalta@gmail.com>
2026-04-26 21:24:49 +0530

    Pattern Practice re

--- PatternPrinting/star.java
+++ PatternPrinting/star.java
@@ -17,7 +17,44 @@ public class star {
             System.out.println();}
     */
 
-        for(int i=1; i<
+        /*
+        Pattern 2:
+        *
+        **
+        ***
+        ****
+        *****
+
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");}
+            System.out.println();
+        }
+        */
+
+        /*
+        Pattern 3:
+             *
+            **
+           ***
+          ****
+*/
+
+
+     /*
+        CODE:
+        for(int i=1; i<=4; i++){
+              for(int j=1; j<=4; j++){
+
+                  if(j > 4 - i)
+                      System.out.print("*");
+                  else
+                      System.out.print(" ");
+
+              }
+              System.out.println();
+          }
+*/
 
 
 }}
