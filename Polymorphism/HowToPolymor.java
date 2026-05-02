e2d5d807bac1713b0ab3611183c106311ce3188f
Krish Jhawalta <krishjhawalta@gmail.com>
2026-05-02 21:49:25 +0530

    Multi polymorphism

--- Polymorphism/HowToPolymor.java
+++ Polymorphism/HowToPolymor.java
@@ -3,23 +3,33 @@ package Polymorphism;
 //Polymorphism is the ability of a method or object to take many forms and perform different actions
 
 class Animal{
-    void sound(){
+    void sound2(){
         System.out.println("Animal makes sound!!");
 
     }
 }
 
-class Dog extends Animal{
-    void sound(){
+class Dog extends Animal {
+    void sound1() {
         System.out.println("Barks!! ");
     }
 
-        }
+}
+
+
+class Cat extends Dog{
+    void sound(){
+        System.out.println("Meow! Meow!");
+    }
+}
 
 public class HowToPolymor {
     public static void main(String[] args) {
-        Animal a = new Dog();
-        a.sound();
+        Cat c = new Cat();    // Polymorphism
+        c.sound();
+        c.sound1();
+        c.sound2();
+
 
 
 
