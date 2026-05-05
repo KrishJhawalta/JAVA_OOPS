b8578d58911ae8e67f438345e30c247ba3eb2598
Krish Jhawalta <krishjhawalta@gmail.com>
2026-05-05 21:33:13 +0530

    Inheritance Practice

---
+++ Inheritance/testing.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+class testcase1 {
+    System.out.println("I am transporter");
+
+}
+public class testing {
+   public static void main(String[] args) {
+       System.out.println("Hello i am under main function");
+
+    }
+}
