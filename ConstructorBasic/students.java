1994a5b5f6bf29d3d5993e9e6037245d48f3ce2a
Krish Jhawalta <krishjhawalta@gmail.com>
2026-06-01 22:01:04 +0530

    Constructor Basics 1

---
+++ ConstructorBasic/students.java
@@ -0,0 +1,23 @@
+package ConstructorBasic;
+
+class S{
+    int age;
+    String name;
+
+    S(){
+        age = 0;
+        name = null;
+
+    }
+}
+
+public class students {
+    public static void main(String[] args) {
+        S s = new S();
+        System.out.println(s.age);
+
+
+
+    }
+
+}
