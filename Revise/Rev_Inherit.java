74c82236376ca82ac803eec9d5c38b64de1b9232
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-10 21:39:07 +0530

    File 123

---
+++ Revise/Rev_Inherit.java
@@ -0,0 +1,19 @@
+class Test{
+ void sayHello(){
+System.out.println("Greetings, Hello!);}
+}
+class Over extends Test{
+
+System.out.println("Hello user!, I am class Over);}
+
+
+public class Rev_Inherit{
+public static void main(String[]args){
+
+Over o1 = new Over();
+o1.sayHello();
+
+
+
+
+}}
