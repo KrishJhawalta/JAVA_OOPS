ded2ff70cae2a9fadf609e08796fd1db9a0edece
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-11 22:10:21 +0530

    File updated inheritance

--- Revise/Rev_Inherit.java
+++ Revise/Rev_Inherit.java
@@ -1,3 +1,4 @@
+// Single Inheritance
 class Test{
  void sayHello(){
 System.out.println("Greetings, Hello!);}
@@ -5,6 +6,21 @@ System.out.println("Greetings, Hello!);}
 class Over extends Test{
 
 System.out.println("Hello user!, I am class Over);}
+
+
+// Hierarchial Inheritance
+
+class Vehicle{
+void Type(){
+System.out.println("Vehicle Types");}}
+
+class Car extends Vehicle{
+void car(){
+System.out.println("Four Wheeler Car");}}
+
+class Bike extends Vehicle{
+void bike(){
+System.out.println("2 Wheeler");}}
 
 
 public class Rev_Inherit{
