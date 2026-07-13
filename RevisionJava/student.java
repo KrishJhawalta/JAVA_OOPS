34b8a1c9120926c12721200e2333fd22cdc4d23a
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-13 21:47:09 +0530

    constructor base

---
+++ RevisionJava/student.java
@@ -0,0 +1,16 @@
+package RevisionJava;
+
+class Student{
+    int name;
+    Student(){
+        name = "empty";
+
+
+    }
+}
+
+public class student {
+   public static void main(String[] args) {
+
+    }
+}
