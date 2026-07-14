47d2b450a26518a17ef3adf8ffe6cabd157753d4
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-14 21:40:06 +0530

    Method overrding

--- RevisionJava/polymorphism.java
+++ RevisionJava/polymorphism.java
@@ -1,4 +1,6 @@
 class Calculator{
+
+    // method overloading
     static void add(int a, int b){
         System.out.println("Sum: " + (a+b));
     }
@@ -8,12 +10,25 @@ class Calculator{
     }
 }
 
+class Animal{
+    static void sound(){
+        System.out.println("Animal makes a sound");
+    }
+}
 
+class Dog extends Animal{
+    @Override
+    static void sound(){
+        System.out.println("Dog barks");
+    }
+}
 public class polymorphism{
     public static void main(String[]args){
 
         Calculator.add(55,22);
         Calculator.add(55,22,33);
+        Subtraction.sub(55,22);
+        Subtraction.sub(55,22,33);
 
 
 
