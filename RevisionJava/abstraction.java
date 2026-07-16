edfe9974eeb49fcd835ddc63186b17f1ac32d333
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-16 21:55:19 +0530

    Abstraction use

---
+++ RevisionJava/abstraction.java
@@ -0,0 +1,18 @@
+abstract class car{
+
+    abstract void start();
+
+    void horn(){
+        System.out.println("Beep! Beep!");
+    }
+    
+}
+
+
+
+public class abstraction {
+    public static void main(String[] args) {
+        
+    }
+    
+}
