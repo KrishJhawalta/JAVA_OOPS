730ecb59ab95c83a85a63bc5b190727cb96cbb74
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-20 21:41:09 +0530

    Fixed asbtract

--- RevisionJava/abstraction.java
+++ RevisionJava/abstraction.java
@@ -23,7 +23,7 @@ public class abstraction{
     public static void main(String[] args) {
         Animal a1 = new Dog();
         Animal a2 = new Cat();
-
+// obejcts
         a1.sound();
         a1.eat();
         a2.sound();
