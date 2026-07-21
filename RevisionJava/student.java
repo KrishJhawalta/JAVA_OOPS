d97c752de943910a45bcd0957931af04e0e2793c
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-21 21:12:35 +0530

    constructor

--- RevisionJava/student.java
+++ RevisionJava/student.java
@@ -1,15 +1,15 @@
-package RevisionJava;
-
 class Student{
     int name;
 
     Student() {  // constructor created
-        System.out.println("Object Created");
+        System.out.println("Constructor of student class");
     }
 }
 
 public class student {
    public static void main(String[] args) {
+    Student s1 = new Student();  // object of student class
+    Student s2 = new Student();  // object of student class
 
     }
 }
