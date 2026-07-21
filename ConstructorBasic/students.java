5e667e6afa218d8e8d0c82e4ceea47791d45eee9
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-21 21:19:11 +0530

    Student Constructor modified

--- ConstructorBasic/students.java
+++ ConstructorBasic/students.java
@@ -3,10 +3,12 @@ package ConstructorBasic;
 class S{
     int age;      // without constructor
     String name;
+    int rollno;
 
     S(){
         age = 0;
         name = null;
+        rollno = 0;
 
     }
 
@@ -22,6 +24,9 @@ public class students {
     public static void main(String[] args) {
         S s = new S();
         System.out.println(s.age);
+        System.out.println(s.name);
+        System.out.println(s.rollno);
+       
 
 
 
