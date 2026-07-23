0d622ef02fe5dd92970c749ecf4337e0be65562c
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-23 22:09:25 +0530

    Interface Intilization

---
+++ RevisionJava/interface.java
@@ -0,0 +1,20 @@
+interface Students{
+    void wearUniform();
+    void wearID();
+
+}
+
+class Krish implements Students{
+    @Override 
+    public void wearUniform(){
+        
+    }
+
+}
+
+public class interface {
+    public static void main(String[] args) {
+       
+    }
+    
+}
