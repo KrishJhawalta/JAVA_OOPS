d8645100ff07d3dd41dc2b5cbd48b8a86815dd71
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-25 21:27:13 +0530

    Titles of classes

--- RevisionJava/InterfaceQuestion.java
+++ RevisionJava/InterfaceQuestion.java
@@ -2,22 +2,23 @@ interface payment{
     void pay();
 }
 
-class UPI implements payment{
+class UPI implements payment{ // using implements keyword we can implement the interface in the class
     @Override
     public void pay(){
         System.out.println("Payment done using UPI");
     }
 }
 
-class creditcard implements payment{
+class creditcard implements payment{  // using implements keyword we can implement the interface in the class
     @Override
     public void pay(){
         System.out.println("Payment done using credit card");
     }
 }
 
-public class InterfaceQuestion {
+public class InterfaceQuestion { 
     public static void main(String[] args) {
+        // creating object of the class which implements the interface
         UPI u = new UPI();
         creditcard c = new creditcard();
         u.pay();
