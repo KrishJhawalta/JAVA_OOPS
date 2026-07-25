96bfe6a666611e4b4bf8672d22db75bc7df54174
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-25 21:28:30 +0530

    Student file fix

--- RevisionJava/student.java
+++ RevisionJava/student.java
@@ -1,8 +1,12 @@
 class Student{
-    int name;
+    String name;
 
     Student() {  // constructor created
         System.out.println("Constructor of student class");
+    }
+
+    void display() {
+        System.out.println("Name of student is: " + name);
     }
 }
 
@@ -10,6 +14,7 @@ public class student {
    public static void main(String[] args) {
     Student s1 = new Student();  // object of student class
     Student s2 = new Student();  // object of student class
-
+    s1.name = "krish";
+    s1.display();
     }
 }
