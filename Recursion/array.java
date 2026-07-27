0ca415246247f80266742eecc20433c85e805780
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-27 14:47:14 +0530

    arrays

---
+++ Recursion/array.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class array {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = sumArray(arr, arr.length);
+        System.out.println("Sum of array elements: " + sum);
+    }
+
+    private static int sumArray(int[] arr, int length) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sumArray'");
+    }
+    
+}
