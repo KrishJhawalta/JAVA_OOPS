a096e586a8b9c77edc67ab2ffd23e06f67a865bb
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-29 21:34:24 +0530

    2d array

--- Recursion/array.java
+++ Recursion/array.java
@@ -9,7 +9,22 @@ public class array {
 
     private static int sumArray(int[] arr, int length) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sumArray'");
+        throw new UnsupportedOperationException("Unimplemented method 'sumArray'"); 
+
+        // Method 1: Declare and allocate memory (values default to 0 for int)
+int[][] matrix = new int[3][4]; // 3 rows and 4 columns
+
+// Method 2: Declare and initialize with values immediately
+int[][] matrix = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+}; 
+
+   
+   
+   
+   
     }
     
 }
