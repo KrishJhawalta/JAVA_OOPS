fe228e8ccfc03a64e63e1709e4f7ad4049f56c41
Krish Jhawalta <krishjhawalta@gmail.com>
2026-07-30 21:38:46 +0530

    star pattern revision

---
+++ PatternPrinting/starpat.java
@@ -0,0 +1,13 @@
+public class starpat{
+  public static void main(String[]args){
+
+    // normal star 
+    for(int i=1; i<=5; i++){
+      for(int j=1; j<=i; j++){
+        System.out.print();
+
+      }
+      System.out.println("*")
+    }
+  }
+}
